3e5067588490ba66aea45bb1e3071cbda4c1efb9
unknown <abdukaxxormalikov59@gmail.com>
2025-07-26 17:27:21 +0500

    main

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Products from './pages/Products'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <>
+    <Navbar/>
+    </>
+  )
+}
+
+export default App
